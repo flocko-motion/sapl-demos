e292381511dc5ac261e8fc924f21e2ceffe9036a
flocko-motion <florian.metzger@gmail.com>
2020-10-09 12:28:21 +0200

    small changes

--- sapl-demo-reactive/src/main/java/org/demo/MqttPolicyInformationPoint.java
+++ sapl-demo-reactive/src/main/java/org/demo/MqttPolicyInformationPoint.java
@@ -21,40 +21,28 @@ import io.sapl.api.pip.Attribute;
 import io.sapl.api.pip.AttributeException;
 import io.sapl.api.pip.PolicyInformationPoint;
 import io.sapl.api.validation.Number;
-import io.sapl.api.validation.Text;
 import io.sapl.grammar.sapl.impl.Val;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Publisher;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
 import java.time.Duration;
-import java.time.Instant;
-import java.time.OffsetDateTime;
-import java.time.ZoneId;
 import java.util.Map;
 
 // MQTT imports
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
-import org.springframework.integration.dsl.Pollers;
-import org.springframework.integration.endpoint.MessageProducerSupport;
-import org.springframework.integration.handler.LoggingHandler;
 import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
 import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
 import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
-import org.springframework.integration.mqtt.outbound.MqttPahoMessageHandler;
 import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
-import org.springframework.integration.stream.CharacterStreamReadingMessageSource;
-import org.springframework.messaging.MessageHandler;
 
-// @NoArgsConstructor
+@NoArgsConstructor
 @Service
+@Slf4j
 @PolicyInformationPoint(name = MqttPolicyInformationPoint.NAME, description = MqttPolicyInformationPoint.DESCRIPTION)
 public class MqttPolicyInformationPoint {
 
@@ -62,15 +50,10 @@ public class MqttPolicyInformationPoint {
 
     public static final String DESCRIPTION = "Policy Information Point and attributes for retrieving current date and time information";
 
-    private static final Log LOGGER = LogFactory.getLog(MqttPolicyInformationPoint.class);
-
-
-    public MqttPolicyInformationPoint() {
-        LOGGER.info("\n=mqtt PIP construct !!!!11!!1===");
-    }
-
+    // singleton mqtt client factory
     public MqttPahoClientFactory mqttClientFactory;
     public MqttPahoClientFactory getMqttClientFactory() {
+        LOGGER.trace("MQTT: getMqttClientFactory()");
         if(this.mqttClientFactory != null) return this.mqttClientFactory;
         DefaultMqttPahoClientFactory factory = new DefaultMqttPahoClientFactory();
         MqttConnectOptions options = new MqttConnectOptions();
@@ -78,59 +61,44 @@ public class MqttPolicyInformationPoint {
 //		options.setUserName("guest");
 //		options.setPassword("guest".toCharArray());
         factory.setConnectionOptions(options);
-        LOGGER.info("\n=setup FACTORY===");
         this.mqttClientFactory = factory;
         return factory;
     }
-/* Skizzen - funktioniert alles nicht:
 
-    @Bean
-    public IntegrationFlow mqttInFlow() {
-        LOGGER.info("\nBEAN - mqttInFlow");
-        return IntegrationFlows.from(mqttInbound())
-                .transform(p -> p + ", received from MQTT")
-                .handle(logger())
-                .get();
+    @Attribute(docs = "Subscribes to a MQTT channel and emits all incoming messages")
+    public Flux<Val> channel(@Number Val value, Map<String, JsonNode> variables) throws AttributeException {
+        LOGGER.trace("MQTT: channel(..)");
+        try {
+            MqttPahoMessageDrivenChannelAdapter adapter = new MqttPahoMessageDrivenChannelAdapter(
+                    "myConsumer",
+                    getMqttClientFactory(),
+                    "/#");
+            adapter.setCompletionTimeout(5000);
+            adapter.setConverter(new DefaultPahoMessageConverter());
+            adapter.setQos(1);
+
+            Publisher<Message<Object>> flow = IntegrationFlows.from(adapter)
+                    .toReactivePublisher();
+            Flux<Val> flux = Flux.from(flow)
+                    .log()
+//                    .map(i -> Val.of("todo: convert message object to val"));
+                    .map(i -> Val.of("2020-10-07T00:00:59.000Z")); // for testing: send a UTC date and time
+            return flux;
+        } catch (Exception e) {
+            throw new AttributeException("Exception while creating the next ticker value.", e);
+        }
     }
-
-    @Bean
-    public MessageProducerSupport mqttInbound() {
-        LOGGER.info("\nBEAN - mqttInbound");
-        MqttPahoMessageDrivenChannelAdapter adapter = new MqttPahoMessageDrivenChannelAdapter("siSampleConsumer",
-                getMqttClientFactory(), "siSampleTopic");
-        adapter.setCompletionTimeout(5000);
-        adapter.setConverter(new DefaultPahoMessageConverter());
-        adapter.setQos(1);
-        return adapter;
-    }
-
-
-    private LoggingHandler logger() {
-        LoggingHandler loggingHandler = new LoggingHandler("INFO");
-        loggingHandler.setLoggerName("siSample");
-        return loggingHandler;
-    }
- */
-
 
     @Attribute(docs = "Emits every x seconds the current UTC date and time as an ISO-8601 string. x is the passed number value.")
     public Flux<Val> ticker(@Number Val value, Map<String, JsonNode> variables) throws AttributeException {
+        LOGGER.trace("MQTT: ticker(..)");
         try {
             // gebe einene fixen wert zurück - in der zukunft sollte das der mqtt output sein
-            return Flux.interval(Duration.ofSeconds(value.get().asLong())).map(i -> Val.of("2020-10-07T08:34:07.042Z"));
+            return Flux.interval(Duration.ofSeconds(value.get().asLong())).map(i -> Val.of("2020-10-07T00:00:01.000Z"));
         } catch (Exception e) {
             throw new AttributeException("Exception while creating the next ticker value.", e);
         }
     }
 
-    @Attribute(docs = "Listens to MQTT Broker - TODO: pass server details as parameter.")
-    public Flux<Val> channel(@Number Val value, Map<String, JsonNode> variables) throws AttributeException {
-        try {
-            // gebe einene fixen wert zurück - in der zukunft sollte das der mqtt output sein
-            return Flux.interval(Duration.ofSeconds(value.get().asLong())).map(i -> Val.of("2020-10-07T08:34:07.042Z"));
-        } catch (Exception e) {
-            throw new AttributeException("Exception while creating the next ticker value.", e);
-        }
-    }
 
 }
